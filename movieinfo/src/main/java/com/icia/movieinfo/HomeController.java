276f5f49a637c3778ee0fcdf44e8e4f24bb6e5c3
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2024-02-06 15:36:31 +0900

    화면 구현

--- movieinfo/src/main/java/com/icia/movieinfo/HomeController.java
+++ movieinfo/src/main/java/com/icia/movieinfo/HomeController.java
@@ -1,39 +1,29 @@
 package com.icia.movieinfo;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
+import com.icia.movieinfo.service.MovieService;
+
+import lombok.extern.slf4j.Slf4j;
+
+
 @Controller
+@Slf4j
 public class HomeController {
+	@Autowired
+	private MovieService mServ;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@GetMapping("/")
+	public String home(Model model) {
+		log.info("home()");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String view = mServ.getMovieList(model);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return view;
 	}
 	
 }
