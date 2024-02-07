e4c6a91afcdede7088cd29e3cd540cc4ebcecbaf
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2024-02-07 16:11:44 +0900

    등록 기능 완성

--- movieinfo/src/main/java/com/icia/movieinfo/service/MovieService.java
+++ movieinfo/src/main/java/com/icia/movieinfo/service/MovieService.java
@@ -1,14 +1,18 @@
 package com.icia.movieinfo.service;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.fileupload.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.icia.movieinfo.dao.MovieDao;
 import com.icia.movieinfo.dto.MovieDto;
@@ -64,6 +68,62 @@ public class MovieService {
 		pageHtml = paging.makePaging();
 		
 		return pageHtml;
+	}
+	
+	public String insertMovie(List<MultipartFile> files,
+								MovieDto movie,
+								HttpSession session,
+								RedirectAttributes rttr) {
+		log.info("insertMovie()");
+		String msg = null; //DB에 저장 성공/실패 관련 메세지 저장
+		String view = null;// 대상 페이지 지정 변수
+		String upFile = files.get(0).getOriginalFilename();
+		//업로드하는 파일의 이름을 추출.
+		
+		try {
+			if(!upFile.equals("")) {
+				FileUpload(files, session, movie);
+			}
+			mDao.insertMovie(movie);
+			view = "redirect:/?pageNum=1";
+		} catch (Exception e) {//저장 실패인 경우
+			e.printStackTrace();
+			view = "redirect:writeFrm";
+			msg = "작성 실패";
+		}
+			rttr.addFlashAttribute("msg", msg);
+		
+		return view;
+	}
+
+	private void FileUpload(List<MultipartFile> files, 
+							HttpSession session, 
+							MovieDto movie) throws Exception {
+		log.info("fileUpload()");
+		
+		String sysname = null;//변경하는 파일명
+		String oriname = null;//원래 파일명
+		
+		String realPath = session.getServletContext().
+				getRealPath("/");
+		log.info(realPath);
+		realPath += "resources/upload/";
+		File folder = new File(realPath);
+		//isDirectory() : 해당 이름이 폴더가 아니거나 존재하지않으면 false
+		if(folder.isDirectory() == false) {
+			folder.mkdir();//폴더생성 메소드
+		}
+		
+		MultipartFile mf = files.get(0);
+		oriname = mf.getOriginalFilename();
+		
+		sysname = System.currentTimeMillis()
+				+ oriname.substring(oriname.lastIndexOf("."));
+		
+		File file = new File(realPath + sysname);
+		
+		mf.transferTo(file); //하드디스크(경로상의 폴더)에 저장
+		movie.setP_sysname(sysname);
 	}
 
 }
